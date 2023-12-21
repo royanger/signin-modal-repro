ff941b83910b99b601312f839542715d8e75c044
Roy Anger <roy@royanger.com>
2023-12-21 16:57:27 -0500

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { SignInButton, UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div className="h-screen">
+      <UserButton afterSignOutUrl="/" />
+      <SignInButton mode="modal" />
+    </div>
+  )
+}
